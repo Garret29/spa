0c6d15e311cd54c4648ff6cdb8a10747997ac5ca
Garret29 <xseventythreex@gmail.com>
2017-03-20 22:29:28 +0100

    commit

---
+++ 03-JavaScriptBasics/script.js
@@ -0,0 +1,7 @@
+/**
+ * Created by Szymon on 20.03.2017.
+ */
+
+function wyswietl() {
+    alert(new Date());
+}
---
+++ 03-JavaScriptBasics/script2.js
@@ -0,0 +1,9 @@
+/**
+ * Created by Szymon on 20.03.2017.
+ */
+
+var count = 0;
+
+function wyswietl() {
+console.log(count++)
+}
